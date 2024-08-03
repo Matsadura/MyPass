93076970b1aee3b00198f87a30c841f37ecdf2e2
Matsadura <zidanditoo1@gmail.com>
2024-08-03 23:14:57 +0100

    [ADD] register endpoint

---
+++ api/v1/views/auth.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+from api.v1.views import app_views
+from models import storage
+from models.user import User
+from flask import request, jsonify, abort
+
+
+@app_views.route('/register', methods=['POST'])
+def register():
+    """ Registers a new user """
+    if request.method == 'POST':
+        user_info = request.get_json(silent=True)
+        if not user_info:
+            abort(400, 'Not a JSON')
+
+        # TO REMOVE USERNAME LATER
+        required_fields = ['email', 'password', 'first_name', 'last_name', 'username']
+        missing_fields = [field for field in required_fields if field not in user_info]
+        if missing_fields:
+            print(missing_fields)
+            abort(400, f"{', '.join(missing_fields)} can't be empty")
+        existing_user = storage.get_specific(User, 'email', user_info['email'])
+        if existing_user:
+            abort(400, 'Email already exists')
+        new_user = User(**user_info)
+        storage.new(new_user)
+        storage.save()
+        return jsonify(new_user.to_dict()), 201

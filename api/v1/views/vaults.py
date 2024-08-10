8481f0f9626e6da61b16128a809670aed4a6325c
Matsadura <zidanditoo1@gmail.com>
2024-08-10 13:04:12 +0100

    [UPDATE] Add vault_id get method

--- api/v1/views/vaults.py
+++ api/v1/views/vaults.py
@@ -2,6 +2,7 @@
 """ """
 from api.v1.views import app_views
 from models import storage
+from models.account import Account
 from models.vault import Vault
 from models.user import User
 from flask import jsonify, request, abort
@@ -28,3 +29,15 @@ def vaults():
         storage.new(new_vault)
         storage.save()
         return jsonify(new_vault.to_dict()), 201
+
+
+@app_views.route('/vaults/<vault_id>', methods=['GET', 'PUT'])
+def vault_id(vault_id):
+    if request.method == 'GET':
+        obj = storage.get_specific(Vault, 'id', str(vault_id))
+        if not obj:
+            abort(404, 'Vault does not exist')
+        to_dict = obj.to_dict()
+        to_dict.update({"Accounts": storage.count_specific(Account, 'vault_id', vault_id)})
+        return jsonify(to_dict)
+    #TO ADD PUT
